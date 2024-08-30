594c6574a1648b562097ce69ee627bf05e84a095
nthinh.dev <nthinh.dev@gmail.com>
2024-08-31 04:22:51 +0700

    refactor(env): add port .env.example

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,7 @@ type Method = 'POST' | 'GET' | 'PUT' | 'DELETE' | 'PATCH';
 export async function api(endpoint: string, method: Method, body?: any) {
   const url = `http://localhost:${process.env.PORT}${endpoint}`;
   console.log(url);
-  const response = fetch(url, {
+  const response = fetch(process.env.URL + endpoint, {
     method,
     headers: {
       'Content-Type': 'application/json',
